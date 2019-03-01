8516255d8440c85b12d0f9ca67b13a465507e51e
ZhuShuaiJohnson <1109478681@qq.com>
2019-03-01 15:02:42 +0800

    coded by JZS 20190301

--- Cpp/cppLearning/cppLearning/char.c
+++ Cpp/cppLearning/cppLearning/char.c
@@ -2,16 +2,11 @@
 #include <stdlib.h>
 #include <conio.h>
 
+#define CATA(x, y) #x ## #y
+
 int main(void)
 {
-	char ch;
-	int a = 12;
-	while (1)
-	{
-		ch = _getch();
-		printf("%c\n", ch);
-		if (ch == 'q')	break;
-	}
+	printf("%s\n", CATA(123, 456));
 	
 
 	system("pause");
